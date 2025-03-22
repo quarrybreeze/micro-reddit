6a0a689477aaea87ad28bead6f57c82d29e678cd
Terrence Clayton <184171442+quarrybreeze@users.noreply.github.com>
2025-03-21 17:55:00 -0700

    add posts model with associations and validations

---
+++ app/models/post.rb
@@ -0,0 +1,5 @@
+class Post < ApplicationRecord
+  belongs_to :user
+  validates :url, presence: true
+  validates :url, format: { with: URI::DEFAULT_PARSER.make_regexp, message: "is not a valid URL" }
+end
