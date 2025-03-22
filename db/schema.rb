1901c9937bf250b704814636524a1dfd74114175
Terrence Clayton <184171442+quarrybreeze@users.noreply.github.com>
2025-03-21 18:16:21 -0700

    add comments model with associations and validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_22_002836) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_22_010519) do
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "url"
     t.integer "user_id", null: false
@@ -25,5 +35,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_22_002836) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
